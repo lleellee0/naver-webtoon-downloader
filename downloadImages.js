314c3bff767b684ed58adc87e82e5a1c89c91483
lleellee0 <lleellee013@gmail.com>
2018-12-01 23:22:47 +0900

    v0.1.0 메소드 정리, 실패에 대한 재시도

--- downloadImages.js
+++ downloadImages.js
@@ -1,16 +1,26 @@
 const request = require('request');
 const fs = require('fs');
 
-const downloadWebToonImages = (uri, downloadPath, startNumber, endNumber) => { // startNumber is mostly 1
+const paddingNumber = (padString, number) => {
+  let pad = padString;
+  let numberString = "" + number;
+  return pad.substring(0, pad.length - numberString.length) + numberString;
+};
+
+const downloadWebToonImages = (uriArr, no, downloadPath, startNumber, endNumber) => { // startNumber is mostly 1
   for(let i = startNumber; i <= endNumber; i++) {
+    let uri = uriArr[i-1].attribs.src;
     request({
-      uri:uri + i +'.jpg',
+      uri:uri,
       headers: {
         accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         acceptEncoding:'gzip, deflate, sdch',
         acceptLanguage:'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
         connection:'keep-alive',
-        host:'imgcomic.naver.net',
+        /* host:'imgcomic.naver.net', 
+            This line has next ISSUE Error [ERR_TLS_CERT_ALTNAME_INVALID]: 
+            Hostname/IP does not match certificate's altnames: Host: imgcomic.naver.net. is not in the cert's altnames: DNS:*.pstatic.net, DNS:pstatic.net
+        */
         referer:uri + startNumber +'.jpg',
         upgradeInsecureRequests:'1',
         userAgent:'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
@@ -18,17 +28,53 @@ const downloadWebToonImages = (uri, downloadPath, startNumber, endNumber) => { /
       encoding: 'binary'
     }, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        fs.writeFile(`${downloadPath}\\${i}.jpg`, body, 'binary', (err) => {if(err) console.log(err)});
+        fs.writeFile(`${downloadPath}\\${paddingNumber("000", no)}-${paddingNumber("000", i)}.jpg`, body, 'binary', (err) => {if(err) console.log(err)});
       } else {
+        console.log("Connection Timeout. " + no + " " + i + " 재시도 합니다. 남은 재시도 횟수 : 3");
+
+        retryDownloadWebToonImages(uri, no, downloadPath, startNumber, endNumber, i, 3);
+
         console.log(error);
       }
     });
   }
 }
 
+const retryDownloadWebToonImages = (uri, no, downloadPath, startNumber, endNumber, i, retryCount) => { // retry는 3회까지만 실시
+    request({
+      uri:uri,
+      headers: {
+        accept:'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        acceptEncoding:'gzip, deflate, sdch',
+        acceptLanguage:'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
+        connection:'keep-alive',
+        /* host:'imgcomic.naver.net', 
+            This line has next ISSUE Error [ERR_TLS_CERT_ALTNAME_INVALID]: 
+            Hostname/IP does not match certificate's altnames: Host: imgcomic.naver.net. is not in the cert's altnames: DNS:*.pstatic.net, DNS:pstatic.net
+        */
+        referer:uri + startNumber +'.jpg',
+        upgradeInsecureRequests:'1',
+        userAgent:'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
+      },
+      encoding: 'binary'
+    }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        fs.writeFile(`${downloadPath}\\${paddingNumber("000", no)}-${paddingNumber("000", i)}.jpg`, body, 'binary', (err) => {if(err) console.log(err)});
+      } else {
+        console.log("Connection Timeout. " + no + " " + i + " 재시도 합니다. 남은 재시도 횟수 : " + --retryCount);
+
+        if(retryCount > 0)
+          retryDownloadWebToonImages(uri, no, downloadPath, startNumber, endNumber, i, retryCount);
+
+        console.log(error);
+      }
+    });
+}
+
 // ex)
 // downloadWebToonImages(
 //   'http://imgcomic.naver.net/webtoon/570503/163/20170118234836_ae150a0a5f1de3051579f72a0e1b27bb_IMAG01_',
+//   163
 //   '.',
 //   1,
 //   28);
--- findImages.js
+++ findImages.js
@@ -7,20 +7,50 @@ const findWebToonImages = (titleId, no) => { // titleId is Webtoon's id, no is S
     uri:`http://comic.naver.com/webtoon/detail.nhn?titleId=${titleId}&no=${no}`,
   }, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      // console.log(body);
       const $ = cheerio.load(body);
 
       let endNumber = $('.wt_viewer img').length;
-      let uri = $('.wt_viewer img')[0].attribs.src.split('1.jpg')[0];
-      // [ 'http://imgcomic.naver.net/webtoon/570503/163/20170118234836_ae150a0a5f1de3051579f72a0e1b27bb_IMAG01_', '' ]
+      let uriArr = $('.wt_viewer img');
 
+      // [ 'http://imgcomic.naver.net/webtoon/570503/163/20170118234836_ae150a0a5f1de3051579f72a0e1b27bb_IMAG01_1.jpg',
+      // 'http://imgcomic.naver.net/webtoon/570503/163/20170118234836_ae150a0a5f1de3051579f72a0e1b27bb_IMAG01_2.jpg' , ...]
       downloadImages.downloadWebToonImages(
-        uri,
-        '.',
+        uriArr,
+        no,
+        `.\\images`,
         1,
         endNumber
       );
     } else {
+      console.log('err ' + no + "화. 재시도 합니다. 남은 재시도 횟수 : 3");
+      retryFindWebToonImages(titleId, no);
+      console.log(error);
+    }
+  });
+}
+
+const retryFindWebToonImages = (titleId, no, retryCount) => { // titleId is Webtoon's id, no is Sequence's id
+  request({
+    uri:`http://comic.naver.com/webtoon/detail.nhn?titleId=${titleId}&no=${no}`,
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(body);
+
+      let endNumber = $('.wt_viewer img').length;
+      let uriArr = $('.wt_viewer img');
+
+      // [ 'http://imgcomic.naver.net/webtoon/570503/163/20170118234836_ae150a0a5f1de3051579f72a0e1b27bb_IMAG01_1.jpg',
+      // 'http://imgcomic.naver.net/webtoon/570503/163/20170118234836_ae150a0a5f1de3051579f72a0e1b27bb_IMAG01_2.jpg' , ...]
+      downloadImages.downloadWebToonImages(
+        uriArr,
+        no,
+        `.\\images`,
+        1,
+        endNumber
+      );
+    } else {
+      console.log('err ' + no + "화. 재시도 합니다. 남은 재시도 횟수 : " + --retryCount);
+      retryFindWebToonImages(titleId, no, retryCount);
       console.log(error);
     }
   });
@@ -31,3 +61,15 @@ const findWebToonImages = (titleId, no) => { // titleId is Webtoon's id, no is S
 //   570503,
 //   163
 // );
+
+for(let i = 1; i <= 273; i++)
+  setTimeout(() => {
+    console.log(`${i}화 저장중..`);
+    findWebToonImages(570503, i);
+  }, i * 2 * 1000);
+// }, 0);
+  
+
+module.exports = {
+  findWebToonImages: findWebToonImages
+}
